313df207688475770e32c0d1addf048f696eb6af
liyeon <fldus612@gmail.com>
2024-08-21 12:01:23 +0900

    🔨 Script - 탑버튼, JoinUs, 푸터

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,3 @@
-// 상단 이동 버튼
-// 스크롤 이벤트 핸들러
-let lastScrollTop = 0; // 마지막 스크롤 위치를 저장할 변수
-
-$(window).scroll(function() {
-		let scrollTop = $(this).scrollTop(); // 현재 스크롤 위치
-
-		if (scrollTop < lastScrollTop) { // 스크롤이 위로 올라가는 경우
-				if (scrollTop > 100) { // 스크롤 위치가 100px 이상일 때
-						$('.btn-top').addClass('on');
-				} else {
-						$('.btn-top').removeClass('on');
-				}
-		} else { // 스크롤이 아래로 내려가는 경우
-				$('.btn-top').removeClass('on');
-		}
-		lastScrollTop = scrollTop; // 현재 스크롤 위치를 마지막 스크롤 위치로 업데이트
-});
-	
-// 버튼 클릭 시 상단으로 스크롤
-$('.btn-top').on('click',function() {
-		$('html, body').animate({ scrollTop: 0 }, 400);
-		return false; // 클릭 시 기본 동작 방지
-});
 //언어 선택
 $('.btn-lang').click(function (e) {
   $('.lang-list').toggleClass('on');
@@ -43,3 +19,59 @@ ScrollTrigger.create({
 });
 
 //인트로
+
+
+//footer
+ScrollTrigger.create({
+	trigger:'#footer',
+	start:'bottom bottom',//[트리거 기준 시작]  [윈도우 기준&화면 기준]
+	end:'bottom bottom',//[트리거 기준 끝] [윈도우 기준&화면 기준]
+	markers:false,
+	scrub:0,
+	onEnter:function(){
+		$('.sc-banner').removeClass('on');
+	},
+	onLeaveBack:function(){
+		$('.sc-banner').addClass('on');
+	}
+})
+
+
+// 탑버튼
+// 버튼 클릭 시 상단으로 스크롤
+$('.btn-top').on('click',function() {
+	gsap.to(window, { duration: 1, scrollTo: 0 });
+});
+
+ScrollTrigger.create({
+  trigger: ".sc-intro",
+  start: "top top",
+  end: "bottom bottom",
+  endTrigger: "#footer",
+  markers: false,
+  onUpdate: function (e) {// 스크롤이 움직일 때마다 호출
+    direction = e.direction; // 스크롤 방향을 나타냄 아래로 스크롤 하는 경우(1) 위로 스크롤 하는 경우 -1
+    if (direction == 1) {
+      $(".btn-top").removeClass("on");
+    } else {
+      $(".btn-top").addClass("on");
+    }
+  },
+  onLeaveBack: function () {
+    $(".btn-top").removeClass("on");
+  },
+});
+
+ScrollTrigger.create({
+  trigger: ".sc-ground",
+  start: "top top",
+  end: "bottom bottom",
+  endTrigger: "#footer",
+  markers: true,
+  onEnter: function () {
+		$(".btn-top").addClass("bottom");
+  },
+  onLeaveBack: function () {
+    $(".btn-top").removeClass("bottom");
+  },
+});
